9d797a73ff33271d96a6702602901b5d31e32d7e
CandyModerna <72458125+CandyModerna@users.noreply.github.com>
2023-02-02 14:45:03 +0800

    adds style and simple UI, rewrite to app.js

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissor'];
-    let choicearr = Math.floor(Math.random() * 3);
-    return choices[choicearr];
-  }
-  
-  function playerInputs() {
-    let playerinput = prompt('Rock, Paper, Scissor.');
-    return playerinput;
-  }
+var playerSelection;
+var computerSelection;
+var playerInput
 
-  function capitalize(input) {
-    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
-  }
+function playGame(playerSelection) {
+  playerInput = playerSelection
+  computerSelection = getcomputerChoice();
+  var result = selectWinner(playerSelection, computerSelection);
+  displayResult(result);
+}
 
-  function selectWinner(playerSelection, computerSelection) {
-    if (computerSelection === capitalize(playerSelection)) {
-      return "Tie"
-    }
-    if (computerSelection === "Rock" && capitalize(playerSelection) === "Scissor") {
-      return 'Computer Wins! Rock smashes your Scissor!';
-    }
-    if (computerSelection === "Paper" && capitalize(playerSelection) === "Rock") {
-      return 'Computer Wins! Paper wraps your Rock!';
-    }
-    if (computerSelection === "Scissor" && capitalize(playerSelection) === "Paper") {
-      return 'Computer Wins! Scissor cuts your Paper!';
-    }
-    if (computerSelection === "Rock" && capitalize(playerSelection) === "Paper") {
-      return 'You Win! Your Paper wraps Rock!';
-    }
-    if (computerSelection === "Paper" && capitalize(playerSelection) === "Scissor") {
-      return 'You Win! Your Scissor cuts Paper!';
-    }
-    if (computerSelection === "Scissor" && capitalize(playerSelection) === "Rock") {
-      return `You Win! ${capitalize(playerSelection)} smashes Scissor!`;
-    }
-  }
+function getcomputerChoice() {
+  let choice = ['rock', 'paper', 'scissors'];
+  let choicearr = Math.floor(Math.random() * 3);
+  return choice[choicearr];
+}
 
-  function clickMe() {
-    const playerSelection = playerInputs()
-    const computerSelection = getComputerChoice()
-    console.log(selectWinner(playerSelection, computerSelection))
-    alert(selectWinner(playerSelection, computerSelection))
+function selectWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a Tie";
+  } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win";
+  } else {
+    return "You Lose";
   }
+}
+
+function displayResult(result) {
+  let players = capitalize(playerInput);
+  let computers = capitalize(computerSelection);
+  var resultMessage = `${result}! You chose ${players} and the computer chose  ${computers}.`;
+  document.getElementById("result").innerHTML = resultMessage; 
+}
+
+function capitalize(text) {
+ return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
