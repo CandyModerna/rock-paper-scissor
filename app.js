0b69203b81a71d581ccf081c86610638a6ac23c8
CandyModerna <72458125+CandyModerna@users.noreply.github.com>
2023-02-14 14:30:59 +0800

    Add Bo5 rounds and scoreboard function/

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var playerSelection;
 var computerSelection;
-var playerInput
+var playerInput;
+var result;
+var playerscore = "0";
+var computerscore = "0";
 
 function playGame(playerSelection) {
+  if (playerscore == 0 || computerscore == 0) {
+    document.getElementById("roundwinner").innerHTML = "";
+  }
   playerInput = playerSelection
   computerSelection = getcomputerChoice();
-  var result = selectWinner(playerSelection, computerSelection);
+  result = selectWinner(playerSelection, computerSelection);
   displayResult(result);
+  getScore();
+  reset();
 }
 
 function getcomputerChoice() {
   let choice = ['rock', 'paper', 'scissors'];
-  let choicearr = Math.floor(Math.random() * 3);
+  let choicearr = Math.floor(Math.random() * choice.length);
   return choice[choicearr];
 }
 
@@ -19,8 +27,10 @@ function selectWinner(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a Tie";
   } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+    playerscore++
     return "You Win";
   } else {
+    computerscore++
     return "You Lose";
   }
 }
@@ -30,6 +40,23 @@ function displayResult(result) {
   let computers = capitalize(computerSelection);
   var resultMessage = `${result}! You chose ${players} and the computer chose  ${computers}.`;
   document.getElementById("result").innerHTML = resultMessage;
+  document.getElementById("pscore").innerHTML = playerscore;
+  document.getElementById("cscore").innerHTML = computerscore;
+}
+
+function getScore() {
+  if (playerscore == 5) {
+    document.getElementById("roundwinner").innerHTML = `You win the GAME!`;
+  } else if (computerscore == 5) {
+    document.getElementById("roundwinner").innerHTML = `Computer wins the GAME!`;
+  }
+}
+
+function reset() {
+  if (playerscore >= 5 || computerscore >= 5) {
+    playerscore = 0;
+    computerscore = 0;
+  }
 }
 
 function capitalize(text) {
